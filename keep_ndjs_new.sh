f0326c87cb77c1fabcfb190d7679cd7880f91a27
zhengh <zhengh@goodrain.com>
2018-01-08 15:53:19 +0800

    no message

--- keep_ndjs_new.sh
+++ keep_ndjs_new.sh
@@ -4,7 +4,8 @@ NDJS_LIST=/tmp/ndjs-list.$$
 OSS_URL="oss://lang-goodrain-me"
 DOWNLOAD_URL="https://s3.amazonaws.com/heroku-nodejs-bins/node/release/linux-x64"
 TAKE_URL="https://nodebin.herokai.com/v1/node/linux-x64/latest.txt"
-NDJS_LOG_DATE=`[date "+%Y-%m-%d %H:%M:%S"]`
+NDJS_LOG_DATE=`date "+%Y-%m-%d %H:%M:%S"`
+
 
 function prepare() {
     /root/bin/ossutil64 cp -f $OSS_URL/node/ndjs-list $NDJS_LIST
@@ -20,13 +21,12 @@ do
     if [ $newset_ver != $ndjs_ver ];then
         echo "$NDJS_LOG_DATE replacing older node.js package"
         wget $DOWNLOAD_URL/node-v$newset_ver-linux-x64.tar.gz -O /tmp/node-v$newset_ver-linux-x64.tar.gz
-            if [  $? -eq 0  ]
-            then
-                dd_uptate_info
+            if [  $? -eq 0  ];then
+                echo "$NDJS_LOG_DATE : update to node-v$newset_ver-linux-x64.tar.gz success" >> /var/log/ndjs_info.log
             else
-                dd_download_err
+                echo "$NDJS_LOG_DATE : update to node-v$newset_ver-linux-x64.tar.gz fail" >> /var/log/ndjs_info.err
             fi
-        sed "s/$ndjs_ver/$newset_ver/" $NDJS_LIST
+        sed -i "s/$ndjs_ver/$newset_ver/" $NDJS_LIST
         /root/bin/ossutil64 cp -f /tmp/node-v$newset_ver-linux-x64.tar.gz $OSS_URL/node/v${newset_ver}/node-v$newset_ver-linux-x64.tar.gz
         echo "$NDJS_LOG_DATE copy node-v$newset_ver-linux-x64.tar.gz to oss complete"
         rm /tmp/node-v$newset_ver-linux-x64.tar.gz
@@ -34,34 +34,29 @@ do
         echo "$NDJS_LOG_DATE $ndjs_ver is newest version in $ndjs_rep"
     fi
 done
-/root/bin/ossutil64 cp -f $NDJS_LIST ${OSS_URL}/node/ndjs-list
-rm $NDJS_LIST
-dd__normal_info
+    /root/bin/ossutil64 cp -f $NDJS_LIST ${OSS_URL}/node/ndjs-list
+    rm $NDJS_LIST
+}
+
+function msg_tasks() {
+    if [ -f /var/log/ndjs_info.log ];then
+        dd_uptate_info
+        rm /var/log/ndjs_info.log
+    elif [ -f /var/log/ndjs_info.err ];then
+        dd_download_err
+        rm /var/log/ndjs_info.err
+    fi
 }
 
 #钉钉更新提示
 function dd_uptate_info() {
+    log_info=$(cat /var/log/ndjs_info.log)
     curl 'https://oapi.dingtalk.com/robot/send?access_token=6786cba9d28faf7241b3c5de63351c49f8e1286564c49a24058f0d5cb3bc4045' \
     -H 'Content-Type: application/json' \
     -d '{
         "msgtype": "text", 
             "text": {
-                "content": "node.js均为最新版，无需更新"
-            },
-            "at": {
-            "atMobiles": ["17614720053"],
-            }
-        }'
-}
-
-#钉钉提示正常
-function dd__normal_info() {
-    curl 'https://oapi.dingtalk.com/robot/send?access_token=6786cba9d28faf7241b3c5de63351c49f8e1286564c49a24058f0d5cb3bc4045' \
-    -H 'Content-Type: application/json' \
-    -d '{
-        "msgtype": "text", 
-            "text": {
-                "content": "node.js均为最新版，未下载更新"
+                "content": "'"$log_info"'"
             },
             "at": {
             "atMobiles": ["17614720053"],
@@ -71,12 +66,13 @@ function dd__normal_info() {
 
 #钉钉提示未下载成功
 function dd_download_err() {
+    err_info=$(cat /var/log/ndjs_info.err)
     curl 'https://oapi.dingtalk.com/robot/send?access_token=6786cba9d28faf7241b3c5de63351c49f8e1286564c49a24058f0d5cb3bc4045' \
     -H 'Content-Type: application/json' \
     -d '{
         "msgtype": "text", 
             "text": {
-                "content": "node.js下载异常"
+                "content": "'"$err_info"'"
             },
             "at": {
             "atMobiles": ["17614720053"],
@@ -85,4 +81,5 @@ function dd_download_err() {
 }
 
 prepare
-check
+check 
+msg_tasks
